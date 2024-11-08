a1f523855ece721a67e9e4417df8a98065ef2cc1
ник сидельников <mikolakran@gmail.com>
2024-11-08 20:59:53 +0300

    sorting from less to more

--- Course_C-/Run.cpp
+++ Course_C-/Run.cpp
@@ -1,11 +1,61 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-extern "C" int additions(int a, int b);
+extern "C" void sortArrayAsm(int array[], int sizeArray);
+
+void randArray(int array[], int lenghtArray);
+void sortArray(int array[], int lenghtArray);
 
 int main(void)
 {
-    int a = additions(4,5);
-    cout << "Hello Word!" << endl;
-    cout << "a + b = " << a << endl;
+	setlocale(LC_ALL, "Russian");
+
+	const int MAX_SIZE_ARRAY = 100000;
+
+	int array[MAX_SIZE_ARRAY] = {};
+	int sizeArray = sizeof(array) / sizeof(*array);
+
+	randArray(array, sizeArray);
+	unsigned int start_time = clock(); // начальное время
+
+	sortArrayAsm(array, sizeArray);
+
+	unsigned int end_time = clock(); // конечное время
+	unsigned int search_time = end_time - start_time; // искомое время
+	cout << search_time << endl;
+}
+
+void randArray(int array[], int lenghtArray)
+{
+	srand(time(NULL));
+	for (int i = 0; i < lenghtArray; i++)
+	{
+		 array[i] = rand() % lenghtArray;
+	}
+}
+
+void sortArray(int array[], int lenghtArray)
+{
+	int a = 0;
+	int b = 0;
+	int x = 0;
+	for (int i = 0; i < lenghtArray; i++)
+	{
+		a = array[i];
+		for (int j = x; j < lenghtArray; j++)
+		{
+			if (array[j] <= array[i])
+			{
+				if (a >= array[j])
+				{
+					a = array[j];
+					b = j;
+				}
+			}
+		}
+		array[b] = array[i];
+		array[i] = a;
+		x++;
+	}
 }
--- Course_C-/TestCodeAsm.asm
+++ Course_C-/TestCodeAsm.asm
@@ -1,17 +1,66 @@
+.data
+   p qword ?
+   a dword 0
+   b dword 0
+   c dword 0
+
+   i dword 0
+   j dword 0
+   x dword 0
 .code
 
-additions proc
-; int additions(int a, int b)
-; RCX - a
-; RDX - b
-; Возврат: RAX
+sortArrayAsm proc 
+; void sortArrayAsm(int array[], int sizeArray) RCX - *array ,RDX - sizeArray
 
-	mov eax, ecx
-	add eax, edx
+	lea rbx,[rcx]
 
-	ret
+for_I:
+	cmp i,edx
+	mov esi,i
+jae for_I_End
+	mov eax,[rbx+(rsi*4)]
+	mov a,eax
 
-additions endp
+	mov eax,x
+	mov j,eax
+for_J:
+	cmp j,edx
+jae for_J_End
+	mov ecx,j
+	inc j
+	mov eax,[rbx+(rcx*4)]
+	cmp eax,[rbx+(rsi*4)]
+jbe if_J1
+jae end_If
+if_J1:
+	mov eax,a
+	cmp eax,[rbx+(rcx*4)]
+jae if_J2
+jbe end_If
+if_J2:
+	mov eax,[rbx+(rcx*4)]
+	mov a,eax
+	dec j
+	mov eax,j
+	inc j
+	mov b,eax
+end_If:
+jmp for_J
 
+for_J_End:
+	mov ecx,i
+	mov eax,[rbx+(rcx*4)]
+	mov esi,b
+	mov [rbx+(rsi*4)],eax
+	mov eax,a
+	mov [rbx+(rcx*4)],eax
+	inc x
+	inc i
+jmp for_I
+
+for_I_End:
+ret
+
+sortArrayAsm endp
 
 end
