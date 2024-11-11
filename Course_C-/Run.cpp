eb3146ff7463ad30c2d6290f74600570955424e6
ник сидельников <mikolakran@gmail.com>
2024-11-11 10:42:49 +0300

    pyramid

--- Course_C-/Run.cpp
+++ Course_C-/Run.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <string>
 #include "codewars.h"
+#include <vector>
 using namespace std;
-
-int digital_root(int n);
 
 int main()
 {
-	cout << digital_root(16) << endl;
+	unsigned int nFloors = 3;
+	vector<string> pyramid;
+	for (int i = 0; i < nFloors; i++)
+	{
+		int size_nFloors = nFloors * 2 - 1;
+		int r = (size_nFloors / 2) - i;
+		string text = "";
+
+		for (int i = 0; i < size_nFloors; i++)
+		{
+			if (i<r || i>=(size_nFloors - r))
+			{
+				text += " ";
+			}
+			else
+			{
+				text += "*";
+			}
+		}
+		pyramid.push_back(text);
+	}
+
+	for (int i = 0; i < nFloors; i++)
+	{
+		string s = pyramid[i];
+		cout << s << endl;
+	}
 }
