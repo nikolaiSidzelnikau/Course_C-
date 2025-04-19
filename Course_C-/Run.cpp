a5bf5cb786d8711a42f1059d848068a9e89157c8
ник сидельников <mikolakran@gmail.com>
2025-04-19 15:16:58 +0300

    45

--- Course_C-/Run.cpp
+++ Course_C-/Run.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 #include "ravesli.h"
+#include <vector>
+#include <string>
 using namespace std;
+
+class People
+{
+private:
+	string name = "";
+	int age = 0;
+public:
+	string get_name()
+	{
+		return name;
+	}
+
+	int get_age()
+	{
+		return age;
+	}
+
+	void set_name(string name)
+	{
+		if (name != "")
+		{
+			this->name = name;
+		}
+	}
+
+	void set_age(int age)
+	{
+		if (age != 0)
+		{
+			this->age = age;
+		}
+	}
+};
 
 int main()
 {
-	returnSequence();
+	People people;
+	people.set_age(18);
+	people.set_name("Nikol");
+	cout << people.get_age() << " " << people.get_name() << endl;
 }
